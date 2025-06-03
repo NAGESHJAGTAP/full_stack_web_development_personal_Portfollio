c264f6b0f5f68f47a54a4beda7801817a9f8a7f8
Nageshjagtap <nageshjagtap063@gmail.com>
2025-06-03 21:55:49 +0530

    add file

---
+++ full_stack_web_development_personal_Portfollio/portfollio/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Portfolio from './Component/Portfollio.jsx'
+function App() {
+  return (
+    <>
+        <Portfolio/>
+    </>
+  )
+}
+
+export default App
